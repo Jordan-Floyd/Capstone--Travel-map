44b89a98f8fccf2b16cb269f437926bad73a50c4
Ilovealtapow <Jfloyd421@gmail.com>
2021-11-14 17:47:12 -0700

    save1

---
+++ backend/routes/pins.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const Pin = require("../models/Pin")
+
+
+// creating pin
+
+router.post("/", async (req,res)=>{
+    const newPin = new Pin(req.body)
+    try{
+        const savedPin = await newPin.save();
+        res.status(200).json(savedPin);
+    }catch(err){
+        res.status(500).json(err)
+    };
+});
+
+// getting pins
+
+router.get("/", async (req,res) =>{
+    try{
+        const pins = await Pin.find();
+    }catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+
+
+
+module.exports = router
